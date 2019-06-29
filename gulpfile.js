cf317e1617fd6fd2e15cbb86cbed7569b5151f6d
skrnvn <skrnvn@gmail.com>
2019-06-29 07:25:49 +0300

    Add all the files

---
+++ gulpfile.js
@@ -0,0 +1,117 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    csso = require('gulp-csso'),
+    gutil = require('gulp-util'),
+    merge = require('merge-stream'),
+    notify = require('gulp-notify'),
+    ghPages = require('gulp-gh-pages'),
+    imagemin = require('gulp-imagemin'),
+    sourcemaps = require('gulp-sourcemaps'),
+    minifyHTML = require('gulp-minify-html'),
+    spritesmith = require('gulp.spritesmith'),
+    autoprefixer = require('gulp-autoprefixer'),
+    fileinclude = require('gulp-file-include'),
+    browserSync = require('browser-sync').create();
+
+// запуск сервера
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: "7777"
+    });
+
+    gulp.watch(['./**/*.html']).on('change', browserSync.reload);
+
+
+    gulp.watch([
+        './templates/**/*.html',
+        './pages/**/*.html'
+    ], ['fileinclude']);
+
+    gulp.watch('./sass/**/*', ['sass']);
+});
+
+// компіляція sass/scss в css
+gulp.task('sass', function() {
+    gulp.src(['./sass/**/*.scss', './sass/**/*.sass'])
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass({ outputStyle: 'expanded' })
+                .on('error', gutil.log)
+        )
+        .on('error', notify.onError())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./css/'))
+        .pipe(browserSync.stream());
+});
+
+// збірка сторінки з шаблонів
+gulp.task('fileinclude', function() {
+    gulp.src('./pages/**/*.html')
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }).on('error', gutil.log))
+        .on('error', notify.onError())
+        .pipe(gulp.dest('./'))
+});
+
+// зтиснення svg, png, jpeg
+gulp.task('minify:img', function() {
+    // беремо всі картинки крім папки де лежать картинки для спрайту
+    return gulp.src(['./images/**/*', '!./images/sprite/*'])
+        .pipe(imagemin().on('error', gutil.log))
+        .pipe(gulp.dest('./public/images/'));
+});
+
+// зтиснення css
+gulp.task('minify:css', function() {
+    gulp.src('./css/**/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 30 versions'],
+            cascade: false
+        }))
+        .pipe(csso())
+        .pipe(gulp.dest('./public/css/'));
+});
+
+// зтиснення html
+gulp.task('minify:html', function() {
+    var opts = {
+        conditionals: true,
+        spare: true
+    };
+
+    return gulp.src(['./*.html'])
+        .pipe(minifyHTML(opts))
+        .pipe(gulp.dest('./public/'));
+});
+
+// створення спрайту з картинок з папки images/sprite
+gulp.task('sprite', function() {
+    var spriteData = gulp.src('images/sprite/*.png').pipe(
+        spritesmith({
+            imgName: 'sprite.png',
+            cssName: '_icon-mixin.scss',
+            cssVarMap: function(sprite) {
+                sprite.name = 'icon-' + sprite.name;
+            }
+        })
+    );
+
+    var imgStream = spriteData.img.pipe(gulp.dest('images/'));
+    var cssStream = spriteData.css.pipe(gulp.dest('sass/'));
+
+    return merge(imgStream, cssStream);
+});
+
+// при виклику в терміналі команди gulp, буде запущені задачі
+// server - для запупуску сервера,
+// sass - для компіляції sass в css, тому що браузер
+// не розуміє попередній синтаксис,
+// fileinclude - для того щоб з маленьких шаблонів зібрати повну сторінку
+gulp.task('default', ['server', 'sass', 'fileinclude'])
+gulp.task('production', ['minify:html', 'minify:css', 'minify:img']);
+
